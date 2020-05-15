7657cae04092f5d4dd580e0e4349fc16a43d80e2
William Murari <williammu6@gmail.com>
2020-05-14 21:57:07 -0300

    email confirmation

---
+++ src/modules/user/Register.ts
@@ -0,0 +1,33 @@
+import { Resolver, Query, Mutation, Arg, UseMiddleware } from "type-graphql";
+import * as bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
+import { RegisterInput } from "./register/RegisterInput";
+import { isAuth } from "../middleware/isAuth";
+import { sendEmail } from "../utils/sendEmail";
+import { createConfirmationUrl } from "../utils/createConfirmationUrl";
+
+@Resolver()
+export class RegisterResolver {
+  @UseMiddleware(isAuth)
+  @Query(() => String)
+  async hello() {
+    return "hello world";
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("data") { firstName, lastName, email, password }: RegisterInput
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save();
+
+    await sendEmail(email, await createConfirmationUrl(user.id));
+
+    return user;
+  }
+}
